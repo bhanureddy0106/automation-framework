5637dacdd5228fc693cdc38c3290cd9491033bdf
bhanu <22h51a05k4@cmrcet.ac.in>
2026-05-07 13:29:49 +0530

    Initial commit - automation framework

--- fixtures/browser_fixture.py
+++ fixtures/browser_fixture.py
@@ -1,9 +1,30 @@
-import pytest
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import pytest
+
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("--start-maximized")
+
+    options.add_argument("--disable-notifications")
+
+    options.add_argument("--disable-popup-blocking")
+
+    options.add_argument("--disable-infobars")
+
+    options.add_argument("--remote-allow-origins=*")
+
+    driver = webdriver.Remote(
+
+        command_executor="http://localhost:4444/wd/hub",
+
+        options=options
+    )
+
     yield driver
+
     driver.quit()
--- tests/test_delete_note_api_to_ui.py
+++ tests/test_delete_note_api_to_ui.py
@@ -1,5 +1,4 @@
 import time
-
 from utils.config_loader import load_config
 from api.notes_api import NotesAPI
 from pages.login_page import LoginPage
@@ -12,6 +11,7 @@ def test_ui_created_note_should_appear_in_api(driver):
     login_page = LoginPage(driver)
     notes_page = NotesPage(driver)
     api = NotesAPI(config)
+
     title = f"E2E UI API Note {int(time.time())}"
     description = "Created from UI and verified using API"
 
@@ -22,14 +22,21 @@ def test_ui_created_note_should_appear_in_api(driver):
     # CREATE NOTE (UI)
     notes_page.create_note(title, description, "Work")
 
+    time.sleep(5)
+    driver.refresh()
+    time.sleep(3)
+
     # API VALIDATION
     api.login()
     notes = api.get_notes().json()["data"]
+
     found = False
+
     for note in notes:
         if note["title"] == title and note["description"] == description:
             found = True
             break
+
     assert found, "UI created note not found in API"
 
 
@@ -50,7 +57,10 @@ def test_delete_note_using_api_and_verify_in_ui(driver):
 
     # CREATE NOTE (UI)
     notes_page.create_note(title, description, "Work")
-    time.sleep(2)
+
+    time.sleep(5)
+    driver.refresh()
+    time.sleep(3)
 
     # VERIFY CREATED IN UI
     assert not notes_page.is_note_not_visible(title)
@@ -60,15 +70,19 @@ def test_delete_note_using_api_and_verify_in_ui(driver):
 
     # FIND NOTE IN API
     notes = api.get_notes().json()["data"]
+
     note_id = None
+
     for note in notes:
         if note["title"] == title:
             note_id = note["id"]
             break
+
     assert note_id is not None, "Note not found in API"
 
     # DELETE VIA API
     delete_response = api.delete_note(note_id)
+
     assert delete_response.status_code == 200
 
     # CONFIRM DELETION IN API (ADDED)
