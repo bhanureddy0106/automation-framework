e704b8e9cf24770f17edaa35cfa2f7375ac045d4
bhanu <22h51a05k4@cmrcet.ac.in>
2026-05-07 23:38:23 +0530

    initial commit

--- conftest.py
+++ conftest.py
@@ -1,11 +1,16 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def driver():
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument("--start-maximized")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub",
+        options=options
+    )
+
     yield driver
     driver.quit()
